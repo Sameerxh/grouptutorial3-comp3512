710eae2e697c5e91c2f04d78ed1a5b854768f757
Sameerxh <32047689+Sameerxh@users.noreply.github.com>
2026-03-16 10:50:49 +0000

    added form and fixed errors in line 13(extra bracket)

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $countries = $result->fetchAll(PDO::FETCH_ASSOC);
 
 
 // confirm if user selected a country
-if (isset($_GET['country'])) && $_GET['country'] != 'all') {
+if (isset($_GET['country']) && $_GET['country'] != 'all') {
     // get images for country selected
     $selectedcountry = $_GET['country'];
     $sql = "SELECT * FROM ImageDetails WHERE CountryCode = 'selectedCountry'";
@@ -21,9 +21,24 @@ if (isset($_GET['country'])) && $_GET['country'] != 'all') {
 }
 
 $result = $db->query(sql);
-#image = $result->fetchAll(PDO::FETCH_ASSOC);
+$images = $result->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Travel Images</title>
+</head>
+<body>
+    <form method="get" action="index.php">
+        <label>Countries:</label>
+        <select name="country">
+            <option value="all">All countries</option>
+            <?php foreach ($countries as $country) { ?>
+                <option value="<?php echo $country['CountryCode']; ?>">
+                    <?php echo $country['CountryName']; ?>
+            </option>
+            <?php } ?>
+        </select>
+        <button type="submit">Filter</button>
+</form>
